fd730f2f3a38fb0fe52b3cf6cb8c860ee7e49898
George <abhi.ojha572@gmail.com>
2024-06-22 13:17:27 +0530

    Rendered nexted HTML like structure h1 tag and its sibling tag

---
+++ App.js
@@ -0,0 +1,20 @@
+//{} is an object(to fill attributes of the h1 tag)
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World using React!!!"
+// );
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 as superchild1"),
+    React.createElement("h1", {}, "I am sibling of h1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 as superchild2"),
+    React.createElement("h1", {}, "I am sibling of h1"),
+  ]),
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
