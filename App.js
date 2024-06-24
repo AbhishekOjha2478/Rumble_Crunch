c5f04863cb9d74c6dbc6cd6bd7755336fb30e8f2
George <abhi.ojha572@gmail.com>
2024-06-24 11:24:53 +0530

    Created React app from scratch

--- App.js
+++ App.js
@@ -4,7 +4,8 @@
 //   { id: "heading" },
 //   "Hello World using React!!!"
 // );
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1 as superchild1"),
